8ce618e8f5d3d08ae103028792de613a0f3bfdad
lYesterdaYl <bgmds@126.com>
2018-10-07 21:15:58 -0700

    Initial commit

---
+++ src/File_System.java
@@ -0,0 +1,6 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class File_System {
+
+}
---
+++ src/IO_System.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class IO_System {
+    public static final int L = 64;   // 64 blocks
+    public static final int B = 64;   // 64 bytes per block
+
+    byte[][] ldisk;
+
+    // constructor, create the ldisk
+    public IO_System()
+    {
+        ldisk = new byte[L][B];
+        clear();
+    }
+
+    public void clear()
+    {
+        for (int i = 0; i < L; i++)
+            for (int j = 0; j < B; j++)
+                ldisk[i][j] = 0;
+    }
+    
+}
